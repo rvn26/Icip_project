9041a7a429207eedbdacb0115ad507e6357c9f3c
rvn26 <bigboos2604@gmail.com>
2025-06-13 14:29:32 +0700

    Update user.blade.php

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -82,9 +82,9 @@
                 </div>
 
             </div>
-            <div class="divider"></div>
+           <div class="divider"></div>
             <div class="flex items-center justify-between flex-wrap gap10 wgp-pagination">
-
+                {{ $user->links('pagination::bootstrap-5') }}
             </div>
         </div>
     </div>
